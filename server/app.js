aaff9c1b7937cadcc40f83e1a89eac45280c8b98
Jibsgrl <jbgariel@gmail.com>
2016-01-07 18:51:49 +0100

    fix pump order

--- server/app.js
+++ server/app.js
@@ -41,36 +41,12 @@ function openStream() {
 }
 
 spark.on('login', function() {
-
   console.log("Retreiving data");
-
   //Get device events
   openStream()
-  //spark.getEventStream(false, 'mine', function(data) {
-//
-//    console.log("Defining variables");
-//
-//    var data_json = JSON.stringify(data);
-//    var dataStream = JSON.parse(data_json);
-//
-//    console.log("Defined data verification");
-//
-//    if (typeof dataStream.name != "undefined") {
-//       if (dataStream.name == "dataStream"){
-//        var sensorsData = dataStream.data.split(";");
-//    }
-//
-//    console.log("Event: " + JSON.stringify(data));
-//    insert(data);
-//    console.log("Data inserted");  
-//    console.log("----------------------------");   
-// }
-//activateMotor(20);
-//});
-
 });
 
-var activateMotor = function(motorDuration) {
+function activateMotor(motorDuration) {
 	spark.callFunction('300037000347343138333038','serverPumpOrder',motorDuration,function(err,data){
 		console.log("cannot pump ;)");
 		console.log(err);
--- server/test_motor.js
+++ server/test_motor.js
@@ -1,13 +1,10 @@
 // app.js
-var mongojs = require('mongojs')
 var spark = require('spark');
 
 var config = require('./config');
-var db = mongojs('ceres_db', ['devices', 'devicestream'])
 
 console.log(config);
 console.log(config.mongodb.user_name);
-console.log(config.mongodb.password);
 
 spark.login({username: config.mongodb.user_name, password: config.mongodb.password});
 
@@ -28,9 +25,9 @@ spark.on('login', function() {
   });
 });
 
-var activateMotor = function(motorDuration) {
-	spark.callFunction('300037000347343138333038','pump',motorDuration,function(err,data){
-		console.log("cannot pump ;)");
-		console.log(err);
-	});
+function activateMotor(motorDuration) {
+  spark.callFunction('300037000347343138333038','serverPumpOrder',motorDuration,function(err,data){
+    console.log("cannot pump ;)");
+    console.log(err);
+  });
 };
